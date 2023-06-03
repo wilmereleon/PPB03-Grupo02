b5a517d9a23f8c662c2756fa20b5ad94476e5160
Wílmer E. León Díaz <42726764+wilmereleon@users.noreply.github.com>
2023-06-03 13:25:23 -0500

    Inserción de mnensaje de prueba

--- src/mvcjava/Main.java
+++ src/mvcjava/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+    //Pruebas de «push»
     @Override
     public void start(Stage primaryStage) {
         Model model = new Model();
